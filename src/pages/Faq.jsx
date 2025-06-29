0129a97e59d8362f8a0eeb908783e4397ad645d5
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-06-29 15:09:06 +0600

    improve: Updated pages/Faq.jsx, pages/FeedbackPage.jsx

--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -3,9 +3,9 @@ import { faqs } from "../constant";
 
 const FAQ = () => {
   return (
-    <section className="py-10 px-3 my-10 rounded-xl shadow bg-primary">
-      <div className="max-w-4xl mx-auto">
-        <h1 className="text-3xl md:text-4xl font-bold text-center text-primary mb-12 font-kaushan">
+    <section className="py-5 px-3 my-10 rounded-xl shadow bg-primary/10">
+      <div className="mx-auto">
+        <h1 className="text-xl md:text-3xl font-semibold text-center text-primary mb-3 font-kaushan">
           <Typewriter
             words={["Frequently Asked Questions"]}
             loop={0}
@@ -17,11 +17,11 @@ const FAQ = () => {
           />
         </h1>
 
-        <div className="space-y-6">
+        <div className="space-y-3">
           {faqs.map((faq, idx) => (
             <div
               key={idx}
-              className="bg-white shadow-md hover:shadow-xl transition duration-300 rounded-xl p-6 border-l-4 border-orange-400 hover:border-orange-600 group cursor-pointer"
+              className="shadow-md hover:shadow-xl transition duration-300 rounded-xl p-6 border-l-4 border-orange-400  group cursor-pointer hover-effect bg-gradient-to-br from-blue-500/20 via-sky-200/30 to-cyan-300/20"
             >
               <h3 className="text-xl font-semibold text-slate-500 mb-2 group-hover:text-orange-900 transition-all duration-300">
                 ❓ {faq.question}
--- src/pages/FeedbackPage.jsx
+++ src/pages/FeedbackPage.jsx
@@ -3,9 +3,9 @@ import { feedbacks } from "../constant";
 
 const FeedbackPage = () => {
   return (
-    <div className="max-w-6xl mx-auto p-6 mt-10">
-      <h2 className="text-3xl font-bold text-center mb-8 text-orange-600">
-        ❤️ What Our Users Say
+    <div className="max--6xl mx-auto p-6 mt-10 bg-accent/10">
+      <h2 className="text-3xl font-bold text-center mb-8 text-primary">
+        💙 What Our Users Say
       </h2>
 
       <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
