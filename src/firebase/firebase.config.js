c83b60821448459d1c77e9326e85dc145f6bf3f3
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-06-30 10:43:34 +0600

    improve: Updated firebase/firebase.config.js

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,26 +5,13 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 //email:healthcare  flavor-book
-/* const firebaseConfig = {
+const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
   projectId: import.meta.env.VITE_PROJECTID,
   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
   appId: import.meta.env.VITE_APPID,
-}; */
-const firebaseConfig = {
-  apiKey: "AIzaSyB3PeX13tBoQ65pO9kRKulB8n9PpiZIabg",
-
-  authDomain: "flavor-book-6e9a8.firebaseapp.com",
-
-  projectId: "flavor-book-6e9a8",
-
-  storageBucket: "flavor-book-6e9a8.firebasestorage.app",
-
-  messagingSenderId: "799865113025",
-
-  appId: "1:799865113025:web:54983dd84f850eea1c4740",
 };
 
 // Initialize Firebase
