c3c97fb0213622a8a4edcb6b32d97b6e5d80dcd1
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-08-27 11:13:04 +0600

    refactor: Updated pages/About.jsx, About/AboutCard.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,22 +1,22 @@
-/* import { features } from "../constant";
+import { features } from "../constant";
 import AboutCard from "./About/AboutCard";
 
 const About = () => {
   return (
-    <section className="min-h-[60vh] py-10 px-6 mt-10 border border-purple-500 bg-primary/10 rounded-xl">
+    <section className="min-h-[60vh] py-10 px-6 mt-10 order border-indigo-500 bg-gray-900/80 rounded-xl">
       <div className="max--4xl mx-auto text-center">
-        <h1 className="text-2xl md:text-3xl font-bold text-cyan-800 mb-2">
+        <h1 className="text-2xl md:text-3xl font-bold text-indigo-400 mb-2">
           About FlavorBook
         </h1>
-        <p className="text-lg  text-purple-950 mb-10 leading-relaxed">
-          <span className="font-semibold text-secondary">FlavorBook</span> — a
+        <p className="text-lg text-slate-300 mb-10 leading-relaxed">
+          <span className="font-semibold text-indigo-300">FlavorBook</span> — a
           recipe-sharing haven for passionate foodies. Whether you're a kitchen
           novice or a seasoned chef, FlavorBook connects you with flavors from
           around the world. Discover top-rated dishes, create your own, and
           share the love of cooking!
         </p>
 
-        <div className="grid md:grid-cols-2 gap-3 mt-10">
+        <div className="grid md:grid-cols-2 gap-8 mt-10">
           {features.map((feature, index) => (
             <AboutCard feature={feature} key={index} />
           ))}
@@ -26,4 +26,4 @@ const About = () => {
   );
 };
 
-export default About; */
+export default About;
--- src/pages/About/AboutCard.jsx
+++ src/pages/About/AboutCard.jsx
@@ -1,4 +1,4 @@
-const AboutCard = ({ feature }) => {
+/* const AboutCard = ({ feature }) => {
   return (
     <div
       className={`shadow-lg p-6 rounded-xl border-l-4 ${feature.borderColor} bg-gradient-to-tr from-accent/10 via-sky-100/10  to-primary/20 hover-effect`}
@@ -7,6 +7,21 @@ const AboutCard = ({ feature }) => {
         {feature.icon} {feature.title}
       </h3>
       <p className="text-gray-700">{feature.description}</p>
+    </div>
+  );
+};
+
+export default AboutCard; */
+
+const AboutCard = ({ feature }) => {
+  return (
+    <div
+      className={`shadow-lg p-6 rounded-xl border-l-4 border-indigo-500 bg-gradient-to-tr from-gray-900/70 via-gray-950/60 to-gray-900/80 hover:shadow-indigo-500/40 transition-shadow duration-300`}
+    >
+      <h3 className="text-xl font-semibold mb-2 text-indigo-400">
+        {feature.icon} {feature.title}
+      </h3>
+      <p className="text-slate-300">{feature.description}</p>
     </div>
   );
 };
